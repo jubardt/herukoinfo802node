5d72860599eeed04148932a23a254d6726b5aa57
jubardt <dominiquejubard@gmail.com>
2022-02-03 10:52:13 +0100

    maj du dossier

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const { emit } = require('process');
-var express = require('express');
-var socketIO = require('socket.io');
+const express = require('express');
+const socketIO = require('socket.io');
+var soap = require('soap');
 const port = process.env.PORT || 3000
-const INDEX = '/index.html';
 
 const server = express().use(express.static("public"))
   .listen(port, () => console.log(`Listening on ${port}`));
 
 const io = socketIO(server);
+var args = {};
+var url = 'http://127.0.0.1:8000/?wsdl';
 
-io.on('connect', function(socket){
+io.on('connection', (socket)=>{
     console.log('client connecte');
 
     socket.on('getList',function(){
-        var args = {};
-        var soap = require('soap');
-        var url = 'http://127.0.0.1:8000/?wsdl';
-        /*
         soap.createClient(url, function(err, client) {
             client.carList(args, function(err, result) {
                 console.log(result);
                 io.emit("res",result);
             });
-        });*/
+        });
         
     })
 })
