33647572a2c023e1431f6c61f61df631bc985c91
jubardt <dominiquejubard@gmail.com>
2022-02-12 15:27:07 +0100

    test code omar

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const socketIO = require('socket.io');
 var soap = require('soap');
 const port = process.env.PORT || 3000;
 const app = express();
-const cors = require('cors');
 
 
-const server = express().use(cors()).use(express.static("public"))
+const server = express().use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+}).use(express.static("public"))
   .listen(port, () => console.log(`Listening on ${port}`));
 
 const io = socketIO(server);
