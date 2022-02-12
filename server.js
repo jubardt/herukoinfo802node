df98a52fba1a52806470ce9bb404297eeb8a39d8
jubardt <dominiquejubard@gmail.com>
2022-02-12 16:00:49 +0100

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = express().use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://info802-rest.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
